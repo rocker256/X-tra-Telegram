ddb0773e167765a8ee7fdb5fb87a9f8b79a33174
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-01-04 22:47:22 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -15,7 +15,7 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned message in @XtraTgBot"
 USER_BOT_WARN_ZERO = "**I am currently offline. Please do not SPAM me.You have been blocked by my userbot and it will remain that way until my master unblocks you.** "
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=742506768)\n\n"
-                    "`Hello, this is X-tra-Telegram Security Service.You have found your way here to my master,`"
+                    "`Yo, this is Walter White's bot.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
                     "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
                     "** Send** `/start` ** TWICE so that we can decide why you're here.**")
@@ -38,7 +38,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                     await PREV_REPLY_MESSAGE[chat.id].delete()
                     del PREV_REPLY_MESSAGE[chat.id]
                 pmpermit_sql.approve(chat.id, reason)
-                await event.edit("Approved Nibba [{}](tg://user?id={})".format(firstname, chat.id))
+                await event.edit("Approved [{}](tg://user?id={})".format(firstname, chat.id))
                 await asyncio.sleep(3)
                 await event.delete()
 
@@ -53,10 +53,22 @@ if Var.PRIVATE_GROUP_ID is not None:
         if event.is_private:
             if pmpermit_sql.is_approved(chat.id):
                 pmpermit_sql.disapprove(chat.id)
-                await event.edit(" ███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ \n\nFuck Off Bitch, Now You Can't Message Me..[{}](tg://user?id={})".format(firstname, chat.id))
+                await event.edit(" ███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ \n\nNow You Can't Message Me Now..[{}](tg://user?id={})".format(firstname, chat.id))
                 await asyncio.sleep(3)
                 await event.client(functions.contacts.BlockRequest(chat.id))
-
+                
+    @command(pattern="^.disapprove ?(.*)")
+    async def approve_p_m(event):
+        if event.fwd_from:
+            return
+        replied_user = await event.client(GetFullUserRequest(event.chat_id))
+        firstname = replied_user.user.first_name
+        reason = event.pattern_match.group(1)
+        chat = await event.get_chat()
+        if event.is_private:
+            if pmpermit_sql.is_approved(chat.id):
+                pmpermit_sql.disapprove(chat.id)
+                
     @bot.on(events.NewMessage(incoming=True))
     async def on_new_private_message(event):
         if event.from_id == bot.uid:
@@ -107,9 +119,9 @@ if Var.PRIVATE_GROUP_ID is not None:
                "**Choose one of the following reasons why you are here:**\n\n"
                "**1. To chat with my master.**\n"
                "**2. To spam my master's inbox.**\n"
-               "**3. To send nudes.**\n"
-               "**4. To enquire something.**\n"
-               "**5. To request something.**\n")
+#                "**3. To send nudes.**\n"
+               "**3. To enquire something.**\n"
+               "**4. To request something.**\n")
          ONE = ("__Okay. Your request has been registered.\n **Do not spam my master's inbox.**\nYou can expect a reply within 24 light years. He is a busy man, unlike you probably.__\n\n"
                 "**⚠️ You will be blocked and reported if you spam nibba. ⚠️**\n\n"
                 "__Send__ `/start` __ twice to go back to the main menu.__")
@@ -163,75 +175,75 @@ if Var.PRIVATE_GROUP_ID is not None:
                      await borg.send_message(chat, TWO)
                      await asyncio.sleep(3)
                      await event.client(functions.contacts.BlockRequest(chat_id))
+#              elif y == "3":
+#                  t = await borg.send_message(chat, Nudas)
+#                  if Var.LESS_SPAMMY is not False:
+#                      await response.delete()
+#                      if chat_id in PREV_REPLY_MESSAGE:
+#                          await PREV_REPLY_MESSAGE[chat_id].delete()
+#                          await PREV_REPLY_MESSAGE[userid].delete()
+#                      PREV_REPLY_MESSAGE[chat_id] = t
+#                  response = await conv.get_response(chat)
+#                  x = response.text
+#                  if x == "1":
+#                      await borg.send_message(chat, "`Oh my, you're very much welcome here ;).\nPlease drop your offerings and let my master judge if you have good heart <3.`\n\n **Please don't flood my inbox, we'll have a nice convo once i come back ;D**")
+#                      response = await conv.get_response(chat)
+#                      if not response.text == "/start":
+#                          k = await borg.send_message(chat, LWARN)
+#                          if Var.LESS_SPAMMY is not False:
+#                              if chat_id in PREV_REPLY_MESSAGE:
+#                                  await PREV_REPLY_MESSAGE[chat_id].delete()
+#                              PREV_REPLY_MESSAGE[chat.id] = k
+#                          response = await conv.get_response(chat)
+#                          if not response.text == "/start":
+#                              await borg.send_message(chat, TWO)
+#                              await asyncio.sleep(3)
+#                              await event.client(functions.contacts.BlockRequest(chat_id))
+#                  elif x == "2":
+#                      await borg.send_message(chat, "**You nigga gay af to send a guy like my your male nudes. \nLeave immediately else you become the ultimate gayest gay the gay world has ever seen. I will reply you when i get online.**")
+#                      if Var.LESS_SPAMMY is not False:
+#                          await response.delete()
+#                      response = await conv.get_response(chat)
+#                      if not response.text == "/start":
+#                          o = await borg.send_message(chat, LWARN)
+#                          if Var.LESS_SPAMMY is not False:
+#                              if chat_id in PREV_REPLY_MESSAGE:
+#                                  await PREV_REPLY_MESSAGE[chat_id].delete()
+#                              PREV_REPLY_MESSAGE[chat.id] = o
+#                          response = await conv.get_response(chat)
+#                          if not response.text == "/start":
+#                              await borg.send_message(chat, TWO)
+#                              await asyncio.sleep(3)
+#                              await event.client(functions.contacts.BlockRequest(chat_id))
+#                  elif x == "3":
+#                      await borg.send_message(chat, "`Please decide a gender for yourself before sending your nudes here,\n not that i'm judging if you're a helicopter or a banana but yeah, If you are anything else than a female Homo-Sapien,\n Do not send more messages and let my master see for himself if he wants to talk with you.`")
+#                      if Var.LESS_SPAMMY is not False:
+#                          await response.delete()
+#                      response = await conv.get_response(chat)
+#                      if not response.text == "/start":
+#                          p = await borg.send_message(chat, LWARN)
+#                          if Var.LESS_SPAMMY is not False:
+#                              if chat_id in PREV_REPLY_MESSAGE:
+#                                  await PREV_REPLY_MESSAGE[chat_id].delete()
+#                              PREV_REPLY_MESSAGE[chat.id] = p
+#                          response = await conv.get_response(chat)
+#                          if not response.text == "/start":
+#                              await borg.send_message(chat, TWO)
+#                              await asyncio.sleep(3)
+#                              await event.client(functions.contacts.BlockRequest(chat_id))
+#                  else:
+#                      g = await borg.send_message(chat, "__You have entered an invalid command. Please send__ `/start` __again or do not send another message if you do not wish to be blocked and reported.__")
+#                      if Var.LESS_SPAMMY is not False:
+#                          if chat_id in PREV_REPLY_MESSAGE:
+#                              await PREV_REPLY_MESSAGE[chat_id].delete()
+#                              await PREV_REPLY_MESSAGE[userid].delete()
+#                          PREV_REPLY_MESSAGE[chat.id] = g
+#                      response = await conv.get_response(chat)
+#                      if not response.text.startswith("/start"):
+#                              await borg.send_message(chat, TWO)
+#                              await asyncio.sleep(3)
+#                              await event.client(functions.contacts.BlockRequest(chat_id))
              elif y == "3":
-                 t = await borg.send_message(chat, Nudas)
-                 if Var.LESS_SPAMMY is not False:
-                     await response.delete()
-                     if chat_id in PREV_REPLY_MESSAGE:
-                         await PREV_REPLY_MESSAGE[chat_id].delete()
-                         await PREV_REPLY_MESSAGE[userid].delete()
-                     PREV_REPLY_MESSAGE[chat_id] = t
-                 response = await conv.get_response(chat)
-                 x = response.text
-                 if x == "1":
-                     await borg.send_message(chat, "`Oh my, you're very much welcome here ;).\nPlease drop your offerings and let my master judge if you have good heart <3.`\n\n **Please don't flood my inbox, we'll have a nice convo once i come back ;D**")
-                     response = await conv.get_response(chat)
-                     if not response.text == "/start":
-                         k = await borg.send_message(chat, LWARN)
-                         if Var.LESS_SPAMMY is not False:
-                             if chat_id in PREV_REPLY_MESSAGE:
-                                 await PREV_REPLY_MESSAGE[chat_id].delete()
-                             PREV_REPLY_MESSAGE[chat.id] = k
-                         response = await conv.get_response(chat)
-                         if not response.text == "/start":
-                             await borg.send_message(chat, TWO)
-                             await asyncio.sleep(3)
-                             await event.client(functions.contacts.BlockRequest(chat_id))
-                 elif x == "2":
-                     await borg.send_message(chat, "**You nigga gay af to send a guy like my your male nudes. \nLeave immediately else you become the ultimate gayest gay the gay world has ever seen. I will reply you when i get online.**")
-                     if Var.LESS_SPAMMY is not False:
-                         await response.delete()
-                     response = await conv.get_response(chat)
-                     if not response.text == "/start":
-                         o = await borg.send_message(chat, LWARN)
-                         if Var.LESS_SPAMMY is not False:
-                             if chat_id in PREV_REPLY_MESSAGE:
-                                 await PREV_REPLY_MESSAGE[chat_id].delete()
-                             PREV_REPLY_MESSAGE[chat.id] = o
-                         response = await conv.get_response(chat)
-                         if not response.text == "/start":
-                             await borg.send_message(chat, TWO)
-                             await asyncio.sleep(3)
-                             await event.client(functions.contacts.BlockRequest(chat_id))
-                 elif x == "3":
-                     await borg.send_message(chat, "`Please decide a gender for yourself before sending your nudes here,\n not that i'm judging if you're a helicopter or a banana but yeah, If you are anything else than a female Homo-Sapien,\n Do not send more messages and let my master see for himself if he wants to talk with you.`")
-                     if Var.LESS_SPAMMY is not False:
-                         await response.delete()
-                     response = await conv.get_response(chat)
-                     if not response.text == "/start":
-                         p = await borg.send_message(chat, LWARN)
-                         if Var.LESS_SPAMMY is not False:
-                             if chat_id in PREV_REPLY_MESSAGE:
-                                 await PREV_REPLY_MESSAGE[chat_id].delete()
-                             PREV_REPLY_MESSAGE[chat.id] = p
-                         response = await conv.get_response(chat)
-                         if not response.text == "/start":
-                             await borg.send_message(chat, TWO)
-                             await asyncio.sleep(3)
-                             await event.client(functions.contacts.BlockRequest(chat_id))
-                 else:
-                     g = await borg.send_message(chat, "__You have entered an invalid command. Please send__ `/start` __again or do not send another message if you do not wish to be blocked and reported.__")
-                     if Var.LESS_SPAMMY is not False:
-                         if chat_id in PREV_REPLY_MESSAGE:
-                             await PREV_REPLY_MESSAGE[chat_id].delete()
-                             await PREV_REPLY_MESSAGE[userid].delete()
-                         PREV_REPLY_MESSAGE[chat.id] = g
-                     response = await conv.get_response(chat)
-                     if not response.text.startswith("/start"):
-                             await borg.send_message(chat, TWO)
-                             await asyncio.sleep(3)
-                             await event.client(functions.contacts.BlockRequest(chat_id))
-             elif y == "4":
                  u = await borg.send_message(chat, FOUR)
                  if Var.LESS_SPAMMY is not False:
                      await response.delete()
@@ -243,7 +255,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                          await borg.send_message(chat, TWO)
                          await asyncio.sleep(3)
                          await event.client(functions.contacts.BlockRequest(chat_id))
-             elif y == "5":
+             elif y == "4":
                  v = await borg.send_message(chat,FIVE)
                  if Var.LESS_SPAMMY is not False:
                      await response.delete()
