3affdf5e91d45ab66b84ccb778284f471c08d5df
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-01-05 15:28:47 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,6 +11,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI"
+    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI**"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      f"`My owner`: {DEFAULTUSER}\n")
