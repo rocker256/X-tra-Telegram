3c165d60e8ee9d8d24feb679e0b3e2e4a352f0d2
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-01-06 22:21:22 +0530

    Update __main__.py
    Try this

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -22,20 +22,21 @@ if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.tgbot = None
-    if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("Initiating Inline Bot")
-        # ForTheGreatrerGood of beautification
-        bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN",
-            api_id=Var.APP_ID,
-            api_hash=Var.API_HASH
-        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished with no errors")
-        print("Starting Userbot")
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("Startup Completed")
-    else:
-        bot.start()
+#     if Var.TG_BOT_USER_NAME_BF_HER is not None:
+#         print("Initiating Inline Bot")
+#         # ForTheGreatrerGood of beautification
+#         bot.tgbot = TelegramClient(
+#             "TG_BOT_TOKEN",
+#             api_id=Var.APP_ID,
+#             api_hash=Var.API_HASH
+#         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+#         print("Initialisation finished with no errors")
+#         print("Starting Userbot")
+#         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+#         print("Startup Completed")
+#     else:
+#         bot.start()
+    bot.start()
     
 
 import glob
