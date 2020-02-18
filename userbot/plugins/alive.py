3a7ce67288faa3ee79e4d2d85d0bf5a1c70e8c13
aaruu22 <59931090+aaruu22@users.noreply.github.com>
2020-02-19 00:19:02 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,11 +12,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
 #     await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI**\n"
-#                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+#                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`Database Status: Userbot Working Great!\n\nAlways with you, my master!\n`"
 #                      f"`My owner`: {DEFAULTUSER}\n")
     await alive.client.send_message(
             alive.chat_id,
-            f"Python: 3.7.3\nEdited by @HeisenbergTheDanger\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`My owner`: {DEFAULTUSER}`",
+            f"Python: 3.7.3\nEdited by @CrossFireIsLit\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`My owner`: {DEFAULTUSER}`",
             file="https://i.imgur.com/pU8BE9B.png"
         )
     await alive.delete()
